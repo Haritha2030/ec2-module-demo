28c4435fca8c7cd23a4422deac8d42426ed5ca41
harithac <harithanallagadda4905@gmail.com>
2024-10-09 20:30:48 +0530

    ec2 module

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,6 @@
 module "ec2"{
     source = "../terraform-aws-ec2"
+    # these are variables expected module, not arguments of resource definition
+    # You can also create variables or else you can directly use values here
     ami_id  = var.ami_something
 }
